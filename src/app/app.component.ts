448fe2db56c21e95346e029bbe6235e3c6475a2c
hari-iyer <hari.iyer.cs@gmail.com>
2020-02-19 17:09:53 -0500

    map check-in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { MouseEvent } from '@agm/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  styleUrls: ['./app.component.scss',],
 })
 export class AppComponent {
     // google maps zoom level
